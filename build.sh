6bb60a021fcb09bf17595d8fbc2a9283ca2655e9
inksha <x1a010@163.com>
2026-03-09 14:44:35 +0800

    ci: add github action

--- build.sh
+++
@@ -1,7 +0,0 @@
-platforms=$(jq -r '.platforms[] | "\(.os)-\(.arch)"' manifest.json)
-
-for platform in $platforms
-do
-  IFS="/" read -r GOOS GOARCH <<< "$platform"
-  GOOS=$GOOS GOARCH=$GOARCH go build -o "$output_name" "$plugin"
-done
